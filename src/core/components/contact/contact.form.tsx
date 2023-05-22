0a7ac7e6457c9236d7b5b3615e2fbb793bd5d5f4
xistri <cristian93marti@gmail.com>
2023-05-22 13:09:59 +0200

    add reset formdata after send

--- src/core/components/contact/contact.form.tsx
+++ src/core/components/contact/contact.form.tsx
@@ -1,28 +1,39 @@
-import { useRef, useState } from 'react';
+import { SyntheticEvent, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 const hiddenClass = 'hidden';
 
+const defaultFormData = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+};
+
 export function ContactForm() {
-    const form = useRef<HTMLFormElement>(null);
+    const [formData, setFormData] = useState(defaultFormData);
 
     const [classOkAlert, setClassOkAlert] = useState(hiddenClass);
     const [classErrorAlert, setClassErrorAlert] = useState(hiddenClass);
 
-    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const handleInput = (event: SyntheticEvent) => {
+        const element = event.target as HTMLFormElement;
+        setFormData({ ...formData, [element.name]: element.value });
+    };
 
-        console.log(form.current);
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
         emailjs
             .sendForm(
                 'default_service',
                 'template_l00sne9',
-                form.current as HTMLFormElement,
+                event.target as HTMLFormElement,
                 'Kf3XpsNZV7MwlHpFH'
             )
             .then(
                 (result) => {
+                    setFormData(defaultFormData);
                     setClassOkAlert('');
                     setTimeout(() => {
                         setClassOkAlert(hiddenClass);
@@ -38,11 +49,39 @@ export function ContactForm() {
     };
 
     return (
-        <form ref={form} onSubmit={sendEmail}>
-            <input type="text" placeholder="Name" name="name" required />
-            <input type="email" placeholder="Email" name="email" required />
-            <input type="text" placeholder="Subject" name="subject" required />
-            <textarea placeholder="Mensaje" rows={5} name="message" required />
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                value={formData.name}
+                onInput={handleInput}
+                required
+            />
+            <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={formData.email}
+                onInput={handleInput}
+                required
+            />
+            <input
+                type="text"
+                placeholder="Subject"
+                name="subject"
+                value={formData.subject}
+                onInput={handleInput}
+                required
+            />
+            <textarea
+                placeholder="Mensaje"
+                rows={5}
+                name="message"
+                value={formData.message}
+                onInput={handleInput}
+                required
+            />
             <div className="contactMe__Form__sendButton">
                 <button type="submit">
                     <div>
